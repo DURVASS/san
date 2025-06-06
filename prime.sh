7fc26da6cada5c608869d1ea979c11ab8eccbf80
sundeep <sundeepdurvasula@gmail.com>
2025-06-06 09:01:22 +0530

    firstfile

---
+++ prime.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "enter the number"
+read number
+count=0
+i=1
+while [[ $i -lt $number ]]
+do
+ if [ $(($number%$i)) -eq 0 ]
+  then
+    count=$(($count+1))
+  fi
+  i=$(($i+1))
+done
+if [ $count -lt 2 ]
+then
+   echo "number is prime"
+else
